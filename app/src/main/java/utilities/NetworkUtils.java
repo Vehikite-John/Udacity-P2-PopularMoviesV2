eb3624c79ad5822c2ec3045abca7e8f63359b7c3
John Vehikite <jdavet1244@gmail.com>
2017-01-01 01:21:50 -0800

    recommitted NetworkUtils.java since past commits had sensitive information

---
+++ app/src/main/java/utilities/NetworkUtils.java
@@ -0,0 +1,109 @@
+package utilities;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by jdavet on 12/28/2016.
+ */
+
+public final class NetworkUtils {
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+
+    private static final String POPULAR_MOVIES_URL =
+            "http://api.themoviedb.org/3/movie/popular?api_key=";
+
+    private static final String TOP_RATED_MOVIES_URL =
+            "http://api.themoviedb.org/3/movie/top_rated?api_key=";
+
+    // TODO: INPUT THEMOVIEDB API KEY HERE
+    private static final String API_KEY = "";
+
+    /*
+     * NOTE: These values only effect responses from OpenWeatherMap, NOT from the fake weather
+     * server. They are simply here to allow us to teach you how to build a URL if you were to use
+     * a real API.If you want to connect your app to OpenWeatherMap's API, feel free to! However,
+     * we are not going to show you how to do so in this course.
+     */
+
+    /* The format we want our API to return */
+    private static final String format = "json";
+    /* The units we want our API to return */
+    private static final String units = "metric";
+    /* The number of days we want our API to return */
+    private static final int numDays = 14;
+
+    final static String QUERY_PARAM = "q";
+    final static String LAT_PARAM = "lat";
+    final static String LON_PARAM = "lon";
+    final static String FORMAT_PARAM = "mode";
+    final static String UNITS_PARAM = "units";
+    final static String DAYS_PARAM = "cnt";
+
+    /**
+     * Builds the URL used to talk to the weather server using a location. This location is based
+     * on the query capabilities of the weather provider that we are using.
+     *
+     * @return The URL to use to query the weather server.
+     */
+    public static URL buildPopularMoviesUrl() {
+        Uri builtUri = Uri.parse(POPULAR_MOVIES_URL + API_KEY).buildUpon().build();
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.v(TAG, "Built URI " + url);
+
+        return url;
+    }
+
+    public static URL buildTopRatedMoviesUrl() {
+        Uri builtUri = Uri.parse(TOP_RATED_MOVIES_URL + API_KEY).buildUpon().build();
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.v(TAG, "Built URI " + url);
+
+        return url;
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
